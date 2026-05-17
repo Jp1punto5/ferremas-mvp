ee4803bbe014975d7959936df6cfbea0df2777d8
Juan Pablo Berrios <juan_berrios1996@hotmail.com>
2026-05-17 01:07:31 -0400

    FER-21: se crea clase usuario_model.py, dentro de el se crean 3 funciones, 1 para obtener un usuario con su correo, otra para validar un usuario con clave y correo y la ultima es para crear un nuevo usuario, por defecto, se asigna el rol CLIENTE

---
+++ modelo/usuario_model.py
@@ -0,0 +1,86 @@
+from modelo.conexion import conectar
+
+
+# =========================
+# OBTENER USUARIO POR CORREO
+# =========================
+
+def obtener_usuario_por_correo(correo):
+
+    conexion = conectar()
+
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        SELECT *
+        FROM usuarios
+        WHERE correo = ?
+    """, (correo,))
+
+    usuario = cursor.fetchone()
+
+    conexion.close()
+
+    return usuario
+
+
+# =========================
+# VALIDAR LOGIN
+# =========================
+
+def validar_login(correo, password):
+
+    conexion = conectar()
+
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        SELECT *
+        FROM usuarios
+        WHERE correo = ?
+        AND password_hash = ?
+    """, (correo, password))
+
+    usuario = cursor.fetchone()
+
+    conexion.close()
+
+    return usuario
+
+
+# =========================
+# REGISTRAR USUARIO
+# =========================
+
+def registrar_usuario(
+        nombre_completo,
+        correo,
+        telefono,
+        password
+):
+
+    conexion = conectar()
+
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        INSERT INTO usuarios
+        (
+            nombre_completo,
+            correo,
+            telefono,
+            password_hash,
+            rol
+        )
+        VALUES (?, ?, ?, ?, ?)
+    """, (
+        nombre_completo,
+        correo,
+        telefono,
+        password,
+        'CLIENTE' #De momento por defecto todos los usuarios registrados serán clientes
+    ))
+
+    conexion.commit()
+
+    conexion.close()
