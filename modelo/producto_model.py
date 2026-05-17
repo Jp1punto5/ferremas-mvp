26b9a82a5ee4493e1f6ae9f702a74467a508bbfe
Juan Pablo Berrios <juan_berrios1996@hotmail.com>
2026-05-17 00:37:47 -0400

    FER-27: se crea la clase producto_model

---
+++ modelo/producto_model.py
@@ -0,0 +1,62 @@
+from modelo.conexion import conectar
+
+
+# =========================
+# LISTAR PRODUCTOS
+# =========================
+
+def obtener_productos():
+
+    conexion = conectar()
+
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        SELECT
+            p.codigo_producto,
+            p.nombre,
+            p.descripcion,
+            p.precio,
+            p.stock,
+            c.nombre AS categoria
+        FROM productos p
+        INNER JOIN categorias c
+            ON p.id_categoria = c.id_categoria
+    """)
+
+    productos = cursor.fetchall()
+
+    conexion.close()
+
+    return productos
+
+
+# =========================
+# OBTENER PRODUCTO
+# =========================
+
+def obtener_producto(codigo_producto):
+
+    conexion = conectar()
+
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        SELECT
+            p.codigo_producto,
+            p.nombre,
+            p.descripcion,
+            p.precio,
+            p.stock,
+            c.nombre AS categoria
+        FROM productos p
+        INNER JOIN categorias c
+            ON p.id_categoria = c.id_categoria
+        WHERE p.codigo_producto = ?
+    """, (codigo_producto,))
+
+    producto = cursor.fetchone()
+
+    conexion.close()
+
+    return producto
