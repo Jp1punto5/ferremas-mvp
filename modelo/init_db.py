06ff06b9ec3fca613eaf40119536e67133034e85
Juan Pablo Berrios <juan_berrios1996@hotmail.com>
2026-05-24 14:53:17 -0400

    Se cambia valor "MODELO" a minuscula.

--- modelo/init_db.py
+++ modelo/init_db.py
@@ -7,7 +7,7 @@ conexion = sqlite3.connect('ferremas.db')
 # CREAR TABLAS
 # =========================
 
-with open('MODELO/database.sql', 'r', encoding='utf-8') as archivo_sql:
+with open('modelo/database.sql', 'r', encoding='utf-8') as archivo_sql:
     script_db = archivo_sql.read()
 
 conexion.executescript(script_db)
@@ -18,7 +18,7 @@ print("Tablas creadas correctamente.")
 # INSERTAR DATOS INICIALES
 # =========================
 
-with open('MODELO/seed_data.sql', 'r', encoding='utf-8') as archivo_seed:
+with open('modelo/seed_data.sql', 'r', encoding='utf-8') as archivo_seed:
     script_seed = archivo_seed.read()
 
 conexion.executescript(script_seed)
